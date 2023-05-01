3e44f376f90e6a61aeb0fb355ddf445bcc5735ed
Will_Smith_007 <business@will-smith-007.de>
2023-05-01 22:45:51 +0200

    Bugfix that the game starts with only one team

--- src/main/java/de/will_smith_007/bedwars/lobby_countdown/LobbyCountdownHelper.java
+++ src/main/java/de/will_smith_007/bedwars/lobby_countdown/LobbyCountdownHelper.java
@@ -2,15 +2,20 @@ package de.will_smith_007.bedwars.lobby_countdown;
 
 import de.will_smith_007.bedwars.lobby_countdown.interfaces.ILobbyCountdownHelper;
 import de.will_smith_007.bedwars.schedulers.LobbyCountdownScheduler;
+import de.will_smith_007.bedwars.teams.enums.BedWarsTeam;
+import de.will_smith_007.bedwars.teams.interfaces.ITeam;
 import lombok.NonNull;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class LobbyCountdownHelper implements ILobbyCountdownHelper {
 
     private final LobbyCountdownScheduler LOBBY_COUNTDOWN_SCHEDULER;
+    private final BedWarsTeam[] BED_WARS_TEAMS = BedWarsTeam.values();
 
     public LobbyCountdownHelper(@NonNull LobbyCountdownScheduler lobbyCountdownScheduler) {
         LOBBY_COUNTDOWN_SCHEDULER = lobbyCountdownScheduler;
@@ -23,7 +28,13 @@ public class LobbyCountdownHelper implements ILobbyCountdownHelper {
         final Collection<? extends Player> players = Bukkit.getOnlinePlayers();
         final int playerSize = players.size();
 
+        final List<ITeam> iTeams = Arrays.stream(BED_WARS_TEAMS)
+                .map(BedWarsTeam::getTeam)
+                .filter(team -> team.getPlayers().size() > 0)
+                .toList();
+
         if (playerSize < 2) return;
+        if (iTeams.size() < 2) return;
 
         LOBBY_COUNTDOWN_SCHEDULER.start();
     }
@@ -43,6 +54,20 @@ public class LobbyCountdownHelper implements ILobbyCountdownHelper {
         if (!LOBBY_COUNTDOWN_SCHEDULER.isRunning()) return;
 
         if (players >= 2) return;
+
+        LOBBY_COUNTDOWN_SCHEDULER.stop();
+    }
+
+    @Override
+    public void cancelCountdownIfNotEnoughTeams() {
+        if (!LOBBY_COUNTDOWN_SCHEDULER.isRunning()) return;
+
+        final List<ITeam> iTeams = Arrays.stream(BED_WARS_TEAMS)
+                .map(BedWarsTeam::getTeam)
+                .filter(team -> team.getPlayers().size() > 0)
+                .toList();
+
+        if (iTeams.size() > 1) return;
 
         LOBBY_COUNTDOWN_SCHEDULER.stop();
     }
--- src/main/java/de/will_smith_007/bedwars/lobby_countdown/interfaces/ILobbyCountdownHelper.java
+++ src/main/java/de/will_smith_007/bedwars/lobby_countdown/interfaces/ILobbyCountdownHelper.java
@@ -3,7 +3,10 @@ package de.will_smith_007.bedwars.lobby_countdown.interfaces;
 public interface ILobbyCountdownHelper {
 
     void startCountdownIfEnoughPlayers();
+
     boolean shortenCountdown();
 
     void cancelCountdownIfNotEnoughPlayers(int players);
+
+    void cancelCountdownIfNotEnoughTeams();
 }
